06a757aff096c61940613af74f3e17e254324630
Ranjeev2 <ranjeev.singh21@st.niituniversity.in>
2024-10-03 00:26:49 +0530

    Server Port Change

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({ 
