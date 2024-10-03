c7a1048a85768eb8ce28590a16804a2a011ae504
Zilean12 <aryan.sharma21@st.niituniversity.in>
2024-10-03 15:24:29 +0530

    Updated files

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,12 +1,23 @@
+// // /middleware/authMiddleware.js
+
+// // Middleware to check if the user is authenticated
+// const ensureAuthenticated = (req, res, next) => {
+//     if (req.isAuthenticated()) {
+//       return next();
+//     }
+//     res.status(401).send('You must log in first.');
+//   };
+  
+//   module.exports = { ensureAuthenticated };
+  
 // /middleware/authMiddleware.js
 
 // Middleware to check if the user is authenticated
 const ensureAuthenticated = (req, res, next) => {
-    if (req.isAuthenticated()) {
-      return next();
-    }
-    res.status(401).send('You must log in first.');
-  };
-  
-  module.exports = { ensureAuthenticated };
-  
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.status(401).send('You must log in first.');
+};
+
+module.exports = { ensureAuthenticated };
