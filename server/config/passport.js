4c1b2a29809b6aa18da63874a8bb5b82d471db09
Aryan Sharma <aryan.sharma21@st.niituniversity.in>
2024-10-03 12:24:19 +0530

    Update passport.js

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,3 +1,39 @@
+// const passport = require('passport');
+// const GoogleStrategy = require('passport-google-oauth20').Strategy;
+// const User = require('../models/user');
+
+// passport.serializeUser((user, done) => {
+//   done(null, user.id);
+// });
+
+// passport.deserializeUser((id, done) => {
+//   User.findById(id).then(user => {
+//     done(null, user);
+//   });
+// });
+
+// passport.use(new GoogleStrategy({
+//   clientID: process.env.GOOGLE_CLIENT_ID,
+//   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//   callbackURL: '/auth/google/callback'
+// },
+// async (accessToken, refreshToken, profile, done) => {
+//   try {
+//     const existingUser = await User.findOne({ googleId: profile.id });
+//     if (existingUser) {
+//       return done(null, existingUser);
+//     }
+//     const user = await new User({ 
+//       googleId: profile.id,
+//       name: profile.displayName,
+//       email: profile.emails[0].value
+//     }).save();
+//     done(null, user);
+//   } catch (err) {
+//     done(err, null);
+//   }
+// }));
+
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/user');
@@ -13,8 +49,8 @@ passport.deserializeUser((id, done) => {
 });
 
 passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  clientID: '375956903024-bcs683urro6q9enqfq858n3r7r57djql.apps.googleusercontent.com', // Hardcoded client ID
+  clientSecret: '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', // Hardcoded client secret
   callbackURL: '/auth/google/callback'
 },
 async (accessToken, refreshToken, profile, done) => {
