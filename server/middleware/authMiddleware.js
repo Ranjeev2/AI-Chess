c0fa4063f646d28ff0f69afc944280032ba62917
Rishabh2333g <rishabh.gupta21@st.niituniversity.in>
2024-11-27 10:47:27 +0530

    Updated authMiddleware

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,18 +1,3 @@
-// // /middleware/authMiddleware.js
-
-// // Middleware to check if the user is authenticated
-// const ensureAuthenticated = (req, res, next) => {
-//     if (req.isAuthenticated()) {
-//       return next();
-//     }
-//     res.status(401).send('You must log in first.');
-//   };
-  
-//   module.exports = { ensureAuthenticated };
-  
-// /middleware/authMiddleware.js
-
-// Middleware to check if the user is authenticated
 const ensureAuthenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next();
